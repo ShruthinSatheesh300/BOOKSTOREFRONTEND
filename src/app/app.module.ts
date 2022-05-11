3d09666f12d7a81cde61a68f5f81ae31caa4fdd8
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-11 16:38:40 +0530

    Added modules and  required imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatTabsModule} from '@angular/material/tabs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupLoginComponent } from './components/signup-login/signup-login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { DashoardComponent } from './components/dashoard/dashoard.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupLoginComponent,
+    ForgotPasswordComponent,
+    DashoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
