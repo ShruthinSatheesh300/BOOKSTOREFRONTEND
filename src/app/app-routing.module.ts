d842bf66072b6db3556a651aa7600968acd2607e
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-11 16:37:53 +0530

    Added path for respective components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashoardComponent } from './components/dashoard/dashoard.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { SignupLoginComponent } from './components/signup-login/signup-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'signup-login',component:SignupLoginComponent},
+  {path:'forgetpass',component:ForgotPasswordComponent},
+  {path:'dashboard',component:DashoardComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
