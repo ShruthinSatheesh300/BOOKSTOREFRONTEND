aefbeb7446dbd4439e8868625ae25a859b1fa3f2
ShruthinSatheesh300 <vishnuofch@gmail.com>
2022-05-11 16:39:06 +0530

    Added HttpClient service

---
+++ src/app/Services/http.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+  baseUrl = environment.baseurl
+
+  constructor(private httpClient: HttpClient) { }
+
+  postService(url: string, reqData: any, token: boolean = false, headers: any) {
+    return this.httpClient.post(this.baseUrl + url, reqData, token && headers)
+  }
+}
